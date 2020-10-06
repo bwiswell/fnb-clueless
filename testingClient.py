839110b446396a7ce270185b257c7ef1e629791a
robm1158 <mullins097956@gmail.com>
2020-10-06 04:05:45 -0600

    FNBC-10 Added ability to send objects across UDP

--- testingClient.py
+++ testingClient.py
@@ -1,16 +1,20 @@
 # Save as client.py 
 # Message Sender
 import os
-from socket import *
+import pickle
+import socket
+
 host = "73.243.41.224" # set to IP address of target computer
 port = 87
 addr = (host, port)
-UDPSock = socket(AF_INET, SOCK_DGRAM)
+UDPSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+UDPSock.connect(addr)
 while True:
-    data = input("Enter message to send or type 'exit': ").encode()
-    UDPSock.sendto(data, addr)
-    print(data.decode())
-    if data.decode() == "exit":
+    data = input("Enter message to send or type 'exit': ")
+    data_string = pickle.dumps(data)
+    UDPSock.send(data_string)
+    print(data_string)
+    if data == "exit":
         print("Exiting server")
         break
 UDPSock.close()
--- testingServer.py
+++ testingServer.py
@@ -1,19 +1,26 @@
 # Save as server.py 
 # Message Receiver
 import os
+import pickle
 from socket import *
 host = "192.168.0.177"
 port = 87
 buf = 1024
 addr = (host, port)
-UDPSock = socket(AF_INET, SOCK_DGRAM)
-UDPSock.bind(addr)
-print("Waiting to receive messages...")
+UDPSock = socket(AF_INET,SOCK_STREAM)
+#UDPSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 5)
+UDPSock.bind((host,port))
+UDPSock.listen(1)
+conn, addr = UDPSock.accept()
+print(f"Waiting to receive messages from {addr}...")
 while True:
-    (data, addr) = UDPSock.recvfrom(buf)
-    print("Received message: " + data.decode())
-    if data.decode() == "exit":
+    data = conn.recv(buf)
+    data_var = pickle.loads(data)
+    
+    #(data, addr) = UDPSock.recvfrom(buf)
+    print("Received message: " + data_var)
+    if data_var == "exit":
         print("Exiting server")
         break
-UDPSock.close()
+conn.close()
 os._exit(0)
