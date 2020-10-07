723765c027d099a679b2b7008af3ca56775b760a
robm1158 <mullins097956@gmail.com>
2020-10-06 20:13:30 -0600

    FNBC-10 Added ability to send objects across as serial

--- Client.py
+++ Client.py
@@ -3,18 +3,24 @@
 import os
 import pickle
 import socket
+import Player as pl
 
 host = "73.243.41.224" # set to IP address of target computer
 port = 87
 addr = (host, port)
 UDPSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 UDPSock.connect(addr)
+
+player1 = pl.Player()
+player1.name = "Rob"
+print(player1.name)
+
 while True:
-    data = input("Enter message to send or type 'exit': ")
-    data_string = pickle.dumps(data)
+    #data = input("Enter message to send or type 'exit': ")
+    data_string = pickle.dumps(player1)
     UDPSock.send(data_string)
     print(data_string)
-    if data == "exit":
+    if data_string == "exit":
         print("Exiting server")
         break
 UDPSock.close()
