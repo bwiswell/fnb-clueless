e5b07e557e5999c3c36ce2dcb58596d8c71b9dd4
robm1158 <mullins097956@gmail.com>
2020-10-06 20:13:30 -0600

    FNBC-10 Added ability to send objects across as serial

---
+++ Player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self):
+        name = ""
+        #location = Room Class
+        weapon = ""
+        turnOrder = 0
+    
+    def sendServerMsg(self):
+        print("messages Sent")
--- Server.py
+++ Server.py
@@ -2,23 +2,29 @@
 # Message Receiver
 import os
 import pickle
+import Player as pl
 from socket import *
+
+
 host = "192.168.0.177"
 port = 87
 buf = 1024
 addr = (host, port)
 UDPSock = socket(AF_INET,SOCK_STREAM)
-#UDPSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 5)
 UDPSock.bind((host,port))
 UDPSock.listen(1)
 conn, addr = UDPSock.accept()
+
+player1 = pl.Player()
+
 print(f"Waiting to receive messages from {addr}...")
 while True:
     data = conn.recv(buf)
     data_var = pickle.loads(data)
+    player1 = data_var
     
     #(data, addr) = UDPSock.recvfrom(buf)
-    print("Received message: " + data_var)
+    print("Received message: " + player1.name)
     if data_var == "exit":
         print("Exiting server")
         break
