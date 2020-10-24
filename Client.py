ac734ce33e6fab69ca1d2ade027b97ce63657373
Sahil Sharma <sahil-sharma@outlook.com>
2020-10-24 18:22:23 -0400

    Not Case Sensitive for User Confirmation and Diagnol Movement

--- Client.py
+++ Client.py
@@ -8,13 +8,13 @@ player = pl.Player()
 #message = msgClass.Message()
 ans = "N"
 
-while ans != "Y":
+while ((ans != "Y") & (ans != "y")):
     player.name = input("Enter player name: ")
     ans = input(player.name + " correct? (Y/N): ")
     print("")
 
-menuDict = {"1", "2", "3", "4", "5"}
-menuStrDict = {1: "up", 2: "down", 3: "left", 4: "right"}
+menuDict = {"1", "2", "3", "4", "5", "6"}
+menuStrDict = {1: "up", 2: "down", 3: "left", 4: "right", 5:"diagnol"}
 print("Welcome to FNB-Clueless Game " + player.name + "...")
 
 while True:
@@ -23,7 +23,8 @@ while True:
     print("2) Move Down")
     print("3) Move Left")
     print("4) Move Right")
-    print("5) Exit")
+    print("5) Move Diagnol")
+    print("6) Exit")
     print("")
     move = input("Enter Move: ")
 
@@ -32,8 +33,8 @@ while True:
         ans = input("Option " + move + " selected, correct? (Y/N): ")
         
         # confirms player move
-        if ans == "Y":
-            if move == "5":
+        if ((ans == "Y") | (ans == "y")):
+            if move == "6":
                 print("Exiting...")
                 os._exit(0)
             else:
